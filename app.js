cf68006ec7bb122ab901ba856dec1cc6003ac3a0
Jean-Marc MAESTRATI <jeanmarcmaestrati@gmail.com>
2024-05-15 14:38:00 +0200

    js

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const addNote = document.getElementById("add_note");
   const noteEditor = document.getElementById("note_editor");
   const saveNote = document.getElementById("save_note");
+  const noteList = document.getElementById("notes_list");
 
   addNote.addEventListener("click", () => {
     noteEditor.style.display = "block";
     saveNote.style.display = "block";
+  });
+
+  saveNote.addEventListener("click", () => {
+    const noteText = noteEditor.value.trim();
+    if (noteText !== "") {
+      const li = document.createElement("li");
+      li.textContent = noteText;
+      noteList.appendChild(li);
+      noteEditor.value = "";
+      noteEditor.style.display = "none";
+      saveNote.style.display = "none";
+    }
   });
 });
