81425f95e47fadc0ca17fa8e6135093b29c806d1
Jean-Marc MAESTRATI <jeanmarcmaestrati@gmail.com>
2024-05-21 16:36:29 +0200

    js

--- app.js
+++ app.js
@@ -3,42 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
   const noteEditor = document.getElementById("note_editor");
   const saveNote = document.getElementById("save_note");
   const noteList = document.getElementById("notes_list");
+  const title = document.getElementById("title");
 
   if (noteList) {
     loadNotes();
   }
 
-  if (addNote) {
-    addNote.addEventListener("click", () => {
-      if (noteEditor) noteEditor.style.display = "block";
-      if (saveNote) saveNote.style.display = "block";
-      addNote.style.display = "none";
-    });
-  }
-
   if (saveNote) {
     saveNote.addEventListener("click", () => {
-      if (noteEditor) {
+      if (title && noteEditor) {
+        const titleText = title.value.trim();
         const noteText = noteEditor.value.trim();
         const url = new URL(location.href);
         const id = url.searchParams.get("id");
 
-        if (noteText !== "") {
+        if (titleText || noteText) {
           if (id) {
-            saveNoteById(id, noteText);
+            saveNoteById(id, titleText, noteText);
           } else {
             const uniqId = Math.floor(Date.now() / 1000);
-            const note = { text: noteText, id: uniqId };
+            const note = { id: uniqId, title: titleText, text: noteText };
             saveToLocalStorage(note);
-            const li = document.createElement("li");
-            li.textContent = `${note.text} ${note.id}`;
-            if (noteList) noteList.appendChild(li);
+            loadNotes();
+            noteList.appendChild(li);
           }
-
-          noteEditor.value = "";
-          noteEditor.style.display = "none";
-          saveNote.style.display = "none";
-          if (addNote) addNote.style.display = "block";
         }
       }
     });
@@ -55,23 +43,44 @@ document.addEventListener("DOMContentLoaded", () => {
   function loadNoteById(id) {
     const notes = localStorage.getItem("notes");
     if (notes) {
-      const parsedNotes = JSON.parse(notes);
-      const note = parsedNotes.find((note) => note.id == id);
-      if (noteEditor && note) {
+      let parsedNotes = JSON.parse(notes);
+      let note = parsedNotes.find((note) => note.id == id);
+      if (title && noteEditor && note) {
+        title.value = note.title;
         noteEditor.value = note.text;
-        noteEditor.style.display = "block";
-        saveNote.style.display = "block";
       }
     }
   }
 
-  function saveNoteById(id, noteText) {
+  function saveNoteById(id, titleText, noteText) {
     const notes = localStorage.getItem("notes");
     if (notes) {
-      const parsedNotes = JSON.parse(notes);
-      const noteId = parsedNotes.findIndex((note) => note.id == id);
+      let parsedNotes = JSON.parse(notes);
+      let noteId = parsedNotes.findIndex((note) => note.id == id);
+      parsedNotes[noteId].title = titleText;
       parsedNotes[noteId].text = noteText;
       localStorage.setItem("notes", JSON.stringify(parsedNotes));
+    }
+  }
+
+  function createDeleteButton(id) {
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+      deleteById(id);
+    });
+    return deleteButton;
+  }
+
+  function deleteById(id) {
+    const notes = localStorage.getItem("notes");
+    if (notes) {
+      let parsedNotes = JSON.parse(notes);
+      let noteId = parsedNotes.filter((note) => note.id != id);
+      localStorage.setItem("notes", JSON.stringify(noteId));
+      loadNotes();
     }
   }
 
@@ -89,15 +98,21 @@ document.addEventListener("DOMContentLoaded", () => {
   function loadNotes() {
     const notes = localStorage.getItem("notes");
     if (notes) {
-      const parsedNotes = JSON.parse(notes);
+      let parsedNotes = JSON.parse(notes);
       noteList.innerHTML = "";
       parsedNotes.forEach((note) => {
         const li = document.createElement("li");
-        li.textContent = `${note.text} ${note.id}`;
+        const deleteButton = createDeleteButton(note.id);
+        const saveText = note.title
+          ? note.title
+          : note.text.split(" ").slice(0, 3).join(" ");
+        li.textContent = saveText;
         li.addEventListener("click", (event) => {
           event.preventDefault();
           window.location.href = `detail.html?id=${note.id}`;
         });
+
+        li.appendChild(deleteButton);
         noteList.appendChild(li);
       });
     }
