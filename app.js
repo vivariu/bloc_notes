098a96e556be75402a642cf06d40c82bfbf7d02b
Jean-Marc MAESTRATI <jeanmarcmaestrati@gmail.com>
2024-05-22 14:13:08 +0200

    js

--- app.js
+++ app.js
@@ -1,66 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addNote = document.getElementById("add_note");
   const noteEditor = document.getElementById("note_editor");
   const saveNote = document.getElementById("save_note");
   const noteList = document.getElementById("notes_list");
   const title = document.getElementById("title");
 
+  let allNotes = getNotesFromLocalStorage();
+
   if (noteList) {
-    loadNotes();
+    loadNotes(allNotes);
   }
 
-  if (saveNote) {
-    saveNote.addEventListener("click", () => {
-      if (title && noteEditor) {
-        const titleText = title.value.trim();
-        const noteText = noteEditor.value.trim();
-        const url = new URL(location.href);
-        const id = url.searchParams.get("id");
+  const urlParams = new URLSearchParams(window.location.search);
+  const noteId = urlParams.get("id");
+  if (noteId) {
+    loadNoteById(allNotes, noteId);
+  }
 
-        if (titleText || noteText) {
-          if (id) {
-            saveNoteById(id, titleText, noteText);
-          } else {
-            const uniqId = Math.floor(Date.now() / 1000);
-            const note = { id: uniqId, title: titleText, text: noteText };
-            saveToLocalStorage(note);
-            loadNotes();
-            noteList.appendChild(li);
-          }
-        }
-      }
+  function getNotesFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("notes"));
+  }
+
+  function setNotesToLocalStorage(notes) {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }
+
+  function loadNotes(notes) {
+    noteList.innerHTML = "";
+    notes.forEach((note) => {
+      const li = createNoteListItem(note);
+      noteList.appendChild(li);
     });
   }
 
-  if (window) {
-    const url = new URL(location.href);
-    const id = url.searchParams.get("id");
-    if (id) {
-      loadNoteById(id);
-    }
-  }
-
-  function loadNoteById(id) {
-    const notes = localStorage.getItem("notes");
-    if (notes) {
-      let parsedNotes = JSON.parse(notes);
-      let note = parsedNotes.find((note) => note.id == id);
-      if (title && noteEditor && note) {
-        title.value = note.title;
-        noteEditor.value = note.text;
-      }
+  function loadNoteById(notes, id) {
+    const note = notes.find((note) => note.id == id);
+    if (title && noteEditor && note) {
+      title.value = note.title;
+      noteEditor.value = note.text;
     }
   }
 
   function saveNoteById(id, titleText, noteText) {
-    const notes = localStorage.getItem("notes");
-    if (notes) {
-      let parsedNotes = JSON.parse(notes);
-      let noteId = parsedNotes.findIndex((note) => note.id == id);
-      parsedNotes[noteId].title = titleText;
-      parsedNotes[noteId].text = noteText;
-      localStorage.setItem("notes", JSON.stringify(parsedNotes));
-    }
+    let noteIndex = allNotes.findIndex((note) => note.id == id);
+    allNotes[noteIndex].title = titleText;
+    allNotes[noteIndex].text = noteText;
+    setNotesToLocalStorage(allNotes);
   }
 
   function createDeleteButton(id) {
@@ -69,52 +53,54 @@ document.addEventListener("DOMContentLoaded", () => {
     deleteButton.classList.add("delete-button");
     deleteButton.addEventListener("click", (event) => {
       event.stopPropagation();
-      deleteById(id);
+      deleteNoteById(id);
     });
     return deleteButton;
   }
 
-  function deleteById(id) {
-    const notes = localStorage.getItem("notes");
-    if (notes) {
-      let parsedNotes = JSON.parse(notes);
-      let noteId = parsedNotes.filter((note) => note.id != id);
-      localStorage.setItem("notes", JSON.stringify(noteId));
-      loadNotes();
-    }
+  function deleteNoteById(id) {
+    allNotes = allNotes.filter((note) => note.id != id);
+    setNotesToLocalStorage(allNotes);
+    loadNotes(allNotes);
+  }
+
+  function createNoteListItem(note) {
+    const li = document.createElement("li");
+    const deleteButton = createDeleteButton(note.id);
+    const saveText = note.title
+      ? note.title
+      : note.text.split(" ").slice(0, 3).join(" ");
+    li.textContent = saveText;
+    li.addEventListener("click", () => {
+      window.location.href = `detail.html?id=${note.id}`;
+    });
+    li.appendChild(deleteButton);
+    return li;
   }
 
   function saveToLocalStorage(note) {
-    let notes = localStorage.getItem("notes");
-    if (!notes) {
-      notes = [];
-    } else {
-      notes = JSON.parse(notes);
-    }
-    notes.push(note);
-    localStorage.setItem("notes", JSON.stringify(notes));
+    allNotes.push(note);
+    setNotesToLocalStorage(allNotes);
   }
 
-  function loadNotes() {
-    const notes = localStorage.getItem("notes");
-    if (notes) {
-      let parsedNotes = JSON.parse(notes);
-      noteList.innerHTML = "";
-      parsedNotes.forEach((note) => {
-        const li = document.createElement("li");
-        const deleteButton = createDeleteButton(note.id);
-        const saveText = note.title
-          ? note.title
-          : note.text.split(" ").slice(0, 3).join(" ");
-        li.textContent = saveText;
-        li.addEventListener("click", (event) => {
-          event.preventDefault();
-          window.location.href = `detail.html?id=${note.id}`;
-        });
+  if (saveNote) {
+    saveNote.addEventListener("click", () => {
+      if (title && noteEditor) {
+        const titleText = title.value.trim();
+        const noteText = noteEditor.value.trim();
+        const id = urlParams.get("id");
 
-        li.appendChild(deleteButton);
-        noteList.appendChild(li);
-      });
-    }
+        if (titleText || noteText) {
+          if (id) {
+            saveNoteById(id, titleText, noteText);
+          } else {
+            const uniqId = Math.floor(Date.now() / 1000);
+            const note = { id: uniqId, title: titleText, text: noteText };
+            saveToLocalStorage(note);
+            loadNotes(allNotes);
+          }
+        }
+      }
+    });
   }
 });
